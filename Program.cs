b9dd5376f469570fa0df43f2746bf6a57d734fe9
Emerson Costa de Oliveira <emerson.costa@localiza.com>
2023-05-07 21:36:57 -0300

    CREATE User e Post

--- Program.cs
+++ Program.cs
@@ -2,19 +2,44 @@
 using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 
-    using var context = new BlogDataContext();
 
-    //CREATE
-    context.Users.Add(new User
+namespace Blog
+{
+    class Program
     {
-        Bio = "9x Microsoft MVP",
-        Email = "andre@balto.io",
-        Image = "https://balta.io",
-        Name = "André Baltieri",
-        PasswordHash = "1234",
-        Slug = "andre-baltieri"
-    });
-    context.SaveChanges();
- 
+        static void Main(string[] args)
+        {
+            using var context = new BlogDataContext();
 
+            //CREATE USER
+            // context.Users.Add(new User
+            // {
+            //     Bio = "9x Microsoft MVP",
+            //     Email = "andre@balto.io",
+            //     Image = "https://balta.io",
+            //     Name = "André Baltieri",
+            //     PasswordHash = "1234",
+            //     Slug = "andre-baltieri"
+            // });
+            // context.SaveChanges();
 
+            var user = context.Users.FirstOrDefault();
+            var post = new Post
+            {
+                Author = user,
+                Body = "Meu artigo",
+                Category = new Category
+                {
+                    Name = "Backend",
+                    Slug = "backend"
+                },
+                CreateDate = System.DateTime.Now,
+                Slug = "meu-artigo",
+                Summary = "Neste artigo vamos conferir ...",
+                Title = "Meu artigo"
+            };
+            context.Posts.Add(post);
+            context.SaveChanges();
+        }
+    }
+}
